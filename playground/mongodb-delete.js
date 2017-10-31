2968b7fd17c6a78761bb9c7a633e2dd604e315a5
evoxtorm <evostorm96@gmail.com>
2017-10-31 22:12:18 +0530

    Adding new thing

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -9,7 +9,30 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
 	}
 	console.log('Connected to Mongodb server');
 
-	
+	// deleteMany
+	// db.collection('Todos').deleteMany({text: 'Edit Lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	//deleteOne
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	//findOneAndDelete
+	// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// db.collection('Users').deleteOne({name: 'Ghgs'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndDelete({
+		_id: new ObjectID("59f8a3ddb9e8dfb0a2324523")
+	}).then((results) => {
+		console.log(JSON.stringify(results, undefined, 2));
+	});
 
 	// db.close();
 });
