a4cf0627e20e7bdfa95095269160aa1b4dcf8a2a
evoxtorm <evostorm96@gmail.com>
2017-11-04 00:04:19 +0530

    Modifid Update.js

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -9,13 +9,33 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
 	}
 	console.log('Connected to Mongodb server');
 
-	db.collection('Todos').findOneAndUpdate({
-		_id: new ObjectID('59f896ecb9e8dfb0a2324236')
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('59f896ecb9e8dfb0a2324236')
+	// }, {
+	// 	$set: {
+	// 		completed: true
+	// 	} 
+	// }, {
+	// 	returnOriginal: false
+	// }).then((result) => {
+	// 	console.log(result);
+	// });
+
+
+	db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('59f619df6ab67c36e48516a4')
 	}, {
 		$set: {
-			completed: true
-		}
-	})
+			name: 'Hitesh'
+	},
+		$inc: {
+		age: 1
+	}
+	}, {
+		returnOriginal: false
+	}).then((result) => {
+		console.log(result);
+	});
 
 	// db.close();
 });
