60de1ad60dd6e80f7bad57cf6ea7ba108d393a13
evoxtorm <evostorm96@gmail.com>
2017-11-05 00:15:11 +0530

    Mongoose User changes

--- server/server.js
+++ server/server.js
@@ -6,21 +6,36 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 
-let Todo = mongoose.model('Todo', {
-	text: {
+// let Todo = mongoose.model('Todo', {
+// 	text: {
+// 		type: String,
+// 		required: true,
+// 		minlength: 1,
+// 		trim: true
+// 	},
+// 	completed: {
+// 		type: Boolean,
+// 		default: false
+// 	},
+// 	completedAt: {
+// 		type: Number,
+// 		default: null
+// 	}
+// });
+
+
+let User = mongoose.model('User', {
+	email: {
 		type: String,
 		required: true,
 		minlength: 1,
 		trim: true
-	},
-	completed: {
-		type: Boolean
-	},
-	completedAt: {
-		type: Number
 	}
 });
 
+let newUser = new User({
+	email: ' evostorm96@gmail.com '
+});
 // let newTodo = new Todo({
 // 	text: 'Cook dinner',
 // 	completed: true,
@@ -33,10 +48,18 @@ let Todo = mongoose.model('Todo', {
 // 	console.log('Unable to save todo');
 // });
 
-let otherTodo = new Todo({});
+// let otherTodo = new Todo({
+// 	text: '  Hi   My Name   is   Hitesh'
+// });
 
-otherTodo.save().then((doc) => {
-	console.log(JSON.stringify(doc, undefined,2));
+// otherTodo.save().then((doc) => {
+// 	console.log(JSON.stringify(doc, undefined,2));
+// }, (e) => {
+// 	console.log('Unable to save todo');
+// });
+
+newUser.save().then((doc) => {
+	console.log('New User', doc);
 }, (e) => {
 	console.log('Unable to save todo');
 });
