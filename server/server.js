71762014f8cc0b2894a1c2a37a204b1d3f090686
evoxtorm <evostorm96@gmail.com>
2017-11-12 00:19:04 +0530

    todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+	let todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+
+
+app.listen(3000, () => {
+	console.log('Started on port 3000');
+});
 
 // const mongoose = require('mongoose');
 
