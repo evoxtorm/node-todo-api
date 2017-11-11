3d8e7fa30a3e92aa314ab2048d020434572ff262
evoxtorm <evostorm96@gmail.com>
2017-11-11 23:28:18 +0530

    new

--- server/server.js
+++ server/server.js
@@ -1,67 +1,47 @@
 const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
-const mongoose = require('mongoose');
-
-
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// const mongoose = require('mongoose');
 
 
-// let Todo = mongoose.model('Todo', {
-// 	text: {
-// 		type: String,
-// 		required: true,
-// 		minlength: 1,
-// 		trim: true
-// 	},
-// 	completed: {
-// 		type: Boolean,
-// 		default: false
-// 	},
-// 	completedAt: {
-// 		type: Number,
-// 		default: null
-// 	}
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+
+
+
+
+// l
+
+// let newUser = new User({
+// 	email: ' evostorm96@gmail.com '
 // });
+// // let newTodo = new Todo({
+// // 	text: 'Cook dinner',
+// // 	completed: true,
+// // 	completedAt:1
+// // });
 
+// // newTodo.save().then((doc) => {
+// // 	console.log('Saved todo', doc);
+// // }, (e) => {
+// // 	console.log('Unable to save todo');
+// // });
 
-let User = mongoose.model('User', {
-	email: {
-		type: String,
-		required: true,
-		minlength: 1,
-		trim: true
-	}
-});
+// // let otherTodo = new Todo({
+// // 	text: '  Hi   My Name   is   Hitesh'
+// // });
 
-let newUser = new User({
-	email: ' evostorm96@gmail.com '
-});
-// let newTodo = new Todo({
-// 	text: 'Cook dinner',
-// 	completed: true,
-// 	completedAt:1
-// });
+// // otherTodo.save().then((doc) => {
+// // 	console.log(JSON.stringify(doc, undefined,2));
+// // }, (e) => {
+// // 	console.log('Unable to save todo');
+// // });
 
-// newTodo.save().then((doc) => {
-// 	console.log('Saved todo', doc);
+// newUser.save().then((doc) => {
+// 	console.log('New User', doc);
 // }, (e) => {
 // 	console.log('Unable to save todo');
 // });
-
-// let otherTodo = new Todo({
-// 	text: '  Hi   My Name   is   Hitesh'
-// });
-
-// otherTodo.save().then((doc) => {
-// 	console.log(JSON.stringify(doc, undefined,2));
-// }, (e) => {
-// 	console.log('Unable to save todo');
-// });
-
-newUser.save().then((doc) => {
-	console.log('New User', doc);
-}, (e) => {
-	console.log('Unable to save todo');
-});
