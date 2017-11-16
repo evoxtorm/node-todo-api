0dc16b231b954fc73e74cddc29821db6ff32d082
evoxtorm <evostorm96@gmail.com>
2017-11-16 20:24:44 +0530

    hola

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -12,7 +12,9 @@ const todos = [{
 }];
 
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -33,7 +35,7 @@ describe('POST /todos', () => {
 				}
 
 
-				Todo.find().then((todos) => {
+				Todo.find({text}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -53,9 +55,22 @@ describe('POST /todos', () => {
 				return done(err);
 			}
 			Todo.find().then((todos) => {
-				expect(todos.length).toBe(0);
+				expect(todos.length).toBe(2);
 				done();
 			}).catch((e) => done(e));
 		});
+	});
+});
+
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
 	});
 });
