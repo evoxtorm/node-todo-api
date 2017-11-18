86417a55b17d6fcb0703e4fa1010482934e2b213
evoxtorm <evostorm96@gmail.com>
2017-11-18 23:59:47 +0530

    chanf

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -35,6 +36,21 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req,res) => {
 	var id = req.params.id;
 	
+	if (!ObjectID.isValid(id)) {
+ 		return res.status(404).send(); 	
+ 	}
+
+
+ 	Todo.findById(id).then((todo) => {
+	if (!todo) {
+		return res.status(404).send();
+		}
+			res.send({todo});		
+		}).catch((e) => {
+			res.status(400).send();
+		});
+		
+
 });
 
 app.listen(3000, () => {
