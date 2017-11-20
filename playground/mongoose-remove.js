f34938e81c253ca96c5dcd56b92481c52b3981fc
evoxtorm <evostorm96@gmail.com>
2017-11-20 22:40:19 +0530

    remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,6 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
