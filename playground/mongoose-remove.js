87efac0f9093036fc67ebc1a99db7f9bfd273081
evoxtorm <evostorm96@gmail.com>
2017-11-20 23:58:02 +0530

    removeid

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,3 +4,13 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
+
+
+// Todo.remove({}).then((result) => {
+// 	console.log(result);
+// });
+
+
+Todo.findByIdAndRemove('5a131deddd639e45b8ef15d7').then((todo) => {
+	console.log(todo);
+});
