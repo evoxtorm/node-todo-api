9165451b10b2f5971f104363b45aba850516ff98
evoxtorm <evostorm96@gmail.com>
2017-11-24 21:09:35 +0530

    change

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -113,16 +113,34 @@ describe('GET /todos/:id', () => {
 
 describe('DELETE /todos/:id', () => {
 	it('should remove a todo', (done) => {
+		var hexId = todos[1]._id.toHexString();
 
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo._id).toBe(hexId);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				Todo.findById(hexId).then((todo) => {
+					expect(todo).toNotExist();
+					done();
+				})
+
+			})
 	});
 
-	it('should return 404 if todo is not found', (done) => {
+	// it('should return 404 if todo is not found', (done) => {
 
-	});
+	// });
 
 
-	it('should return 404 if object id is invalid', (done) => {
+	// it('should return 404 if object id is invalid', (done) => {
 
-	});
+	// });
 
 });
