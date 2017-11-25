d61a1d76f2dea949384afb9c420e68b768f44968
evoxtorm <evostorm96@gmail.com>
2017-11-25 20:38:23 +0530

    Final

--- server/server.js
+++ server/server.js
@@ -81,7 +81,22 @@ app.patch('/todos/:id', (req, res) => {
 		return res.status(404).send();
 	}
 
-	
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	})
 });
 
 app.listen(3000, () => {
