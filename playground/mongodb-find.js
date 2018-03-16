ab04dbb21ec36837b87c208b617ed9fb0fac832f
evoxtorm <evostorm96@gmail.com>
2018-03-16 16:56:20 +0530

    New

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -25,12 +25,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
 	// 	console.log('Unable to fetch todos', err);
 	// });
 
+	db.collection('users').find({name: 'Hitesh'}).toArray().then((docs) => {
+		console.log(JSON.stringify(docs, undefined, 2));
+	})
 
-	db.collection('Users').find({}).count().then((count) => {
-		console.log(`Todos count: ${count}`);
-	}, (err) => {
-		console.log('Unable to fetch todos', err);
-	});
+	// db.collection('Users').find({}).count().then((count) => {
+	// 	console.log(`Todos count: ${count}`);
+	// }, (err) => {
+	// 	console.log('Unable to fetch todos', err);
+	// });
 
 	// db.close();
 });
